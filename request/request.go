a429e67e580609df26827effb80a2197b458f17c
João Ferreira <joaovtf39@gmail.com>
2023-08-26 14:04:05 -0300

    feat: add request package

---
+++ request/request.go
@@ -0,0 +1,15 @@
+package request
+
+type rating struct {
+	Rate  float32 `json:"rate"`
+	Count int     `json:"count"`
+}
+
+type Product struct {
+	ID          int     `json:"id"`
+	Title       string  `json:"title"`
+	Price       float32 `json:"price"`
+	Description string  `json:"description"`
+	Category    string  `json:"category"`
+	Rating      *rating `json:"rating"`
+}
