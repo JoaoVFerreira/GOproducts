af7e0bac663101b032de1fe7dd8a7899288428a7
João Ferreira <joaovtf39@gmail.com>
2023-08-26 15:35:33 -0300

    feat: add validate method

--- http/request.go
+++ http/request.go
@@ -1,5 +1,11 @@
 package http
 
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
 type rating struct {
 	Rate  float32 `json:"rate"`
 	Count int     `json:"count"`
@@ -12,4 +18,39 @@ type Product struct {
 	Description string  `json:"description"`
 	Category    string  `json:"category"`
 	Rating      *rating `json:"rating"`
+}
+
+func checkProductParams(param interface{}, typ string, w http.ResponseWriter) {
+	r := Response{
+		Message: "Wrong use of sent body params",
+		StatusCode: http.StatusUnprocessableEntity,
+		Response: fmt.Sprintf("Parameter %s (type %s) could not be empty", param, typ),
+	}
+
+	response, err := json.Marshal(&r); if err != nil {
+		http.Error(w, "Error parsing data", http.StatusInternalServerError)
+	}
+
+	w.Write(response)
+}
+
+func (p *Product) Validate(w http.ResponseWriter) {
+	if p.Title == "" {
+		checkProductParams(p.Title, "string", w)
+	}
+	if p.Price == 0 {
+		checkProductParams(p.Price, "float32", w)
+	}
+	if p.Description == "" {
+		checkProductParams(p.Description, "string", w)
+	}
+	if p.Category == "" {
+		checkProductParams(p.Category, "string", w)
+	}
+	if p.Rating.Count == 0 {
+		checkProductParams(p.Rating.Count, "int", w)
+	}
+	if p.Rating.Rate == 0 {
+		checkProductParams(p.Rating.Rate, "float32", w)
+	}
 }
